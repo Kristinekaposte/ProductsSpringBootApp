d3498d99158038899400a953f1947ff0caf26328
kristine kaposte <kika217@inbox.lv>
2023-01-30 18:33:41 +0200

    4th commit  with css and array list changed to map

--- src/main/java/com/example/productsspringboot/ProductsController.java
+++ src/main/java/com/example/productsspringboot/ProductsController.java
@@ -5,11 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class ProductsController {
    public static Products products;
-    public static ArrayList<Products> productsList = new ArrayList<>();
+  //  public static ArrayList<Products> productsList = new ArrayList<>();
+    public static Map<Integer,Products>productsList =new HashMap<>();
 
     @GetMapping("/addProducts")
     public String addProductsPage(
@@ -23,17 +26,22 @@ public class ProductsController {
         if (productName != null && !productName.isBlank()
                 && quantity != null && price != null
                 && description != null && !description.isBlank()
-                && isAvailable != null
-        ) {
-            productsList.add(products = new Products(id,productName, quantity, price, description, isAvailable));
-            System.out.println("adding new product: " + productsList.toString());
+                && isAvailable != null ) {
+
+                productsList.put(id, new Products(id,productName, quantity, price, description, isAvailable));
+                //  productsList.add(products = new Products(id,productName, quantity, price, description, isAvailable));
+                // System.out.println("adding new product: " + productsList.toString());
+                for (Map.Entry<Integer, Products> entry : productsList.entrySet()) {
+                    System.out.println(entry.getKey() + ":" + entry.getValue().toString());
+            }
+
         }
         return "addProducts";
     }
 
     @GetMapping("/viewProducts")
     public String showProducts(Model model) {
-        model.addAttribute("listOfAllProducts", productsList); //will transfer list
+        model.addAttribute("listOfAllProducts", productsList.values()); //will transfer list
             return "viewProducts";
 
 }
@@ -41,6 +49,7 @@ public class ProductsController {
     @GetMapping("/products/edit/{id}")
     public String editProducts(@PathVariable("id") Integer id,Model model){
         if (id!=null) {
+           // model.addAttribute("editProducts", productsList.get(id)); // recognises only as array index not as id!!!
             model.addAttribute("editProducts", productsList.get(id)); // recognises only as array index not as id!!!
         }
         return "updateProducts";
@@ -49,7 +58,8 @@ public class ProductsController {
     @PostMapping("/products/edit/{id}")
     public String updateEmployee(@ModelAttribute("editProducts") Products products, @PathVariable("id") Integer id,
                                Model model) {
-            model.addAttribute("listOfAllProducts", productsList.set(id, products));
+        model.addAttribute("listOfAllProducts", productsList.put(id, products));
+         //   model.addAttribute("listOfAllProducts", productsList.set(id, products));
         return "redirect:/viewProducts";
     }
 
@@ -57,7 +67,9 @@ public class ProductsController {
     // to delete SOME PRODUCT
     @GetMapping("/products/delete/{id}")
     public String deleteProduct(Model model, @PathVariable("id") Integer id) {
-            model.addAttribute("listOfAllProducts",  productsList.removeIf(products -> id.equals(products.getId())));
+           // model.addAttribute("listOfAllProducts",  productsList.removeIf(products -> id.equals(products.getId())));
+        model.addAttribute("listOfAllProducts",  productsList.remove(id));
+
         return "redirect:/viewProducts";
     }
 }
